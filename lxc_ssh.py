b7d63f9c4b994e358db7be3d2f71e9b7ab6f4533
Robert Timm <mail@rtti.de>
2018-03-24 01:49:29 +0100

    use login shell with lxc-attach

--- lxc_ssh.py
+++ lxc_ssh.py
@@ -1128,7 +1128,7 @@ class Connection(ConnectionBase):
                     % (pipes.quote(h),
                        pipes.quote(cmd))
         elif (self.lxc_version == 1):
-            lxc_cmd = 'lxc-attach --name %s -- /bin/sh -c %s'  \
+            lxc_cmd = 'lxc-attach --name %s -- /bin/sh -l -c %s'  \
                     % (pipes.quote(h),
                        pipes.quote(cmd))
         if in_data:
@@ -1171,7 +1171,7 @@ class Connection(ConnectionBase):
                         % (pipes.quote(h),
                            pipes.quote(cmd))
             elif (self.lxc_version == 1):
-                lxc_cmd = 'lxc-attach --name %s -- /bin/sh -c %s'  \
+                lxc_cmd = 'lxc-attach --name %s -- /bin/sh -l -c %s'  \
                         % (pipes.quote(h),
                            pipes.quote(cmd))
             if in_data:
@@ -1208,7 +1208,7 @@ class Connection(ConnectionBase):
                     % (pipes.quote(h),
                        pipes.quote(cmd))
         elif (self.lxc_version == 1):
-            lxc_cmd = 'lxc-attach --name %s -- /bin/sh -c %s'  \
+            lxc_cmd = 'lxc-attach --name %s -- /bin/sh -l -c %s'  \
                     % (pipes.quote(h),
                        pipes.quote(cmd))
 
